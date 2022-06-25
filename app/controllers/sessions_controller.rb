400bbcf80a3e2ed5c01c9abf3e5659502f06dfde
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-06-25 13:59:22 -0700

    Create user and session controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+	def new 
+		render :new 
+	end
+
+	def create 
+		user = User.find_by_credentials(
+			params[:user][:username], 
+			params[:user][:password]
+		)
+		if user.nil? 
+			flash.now[:errors] << 'Incorrect username and/or password'
+			render :new, status: :unprocessable_entity
+		else 
+			login_user!(user)
+			redirect_to user_url(user)
+		end
+	end
+
+	def destroy 
+		logout_user!(current_user)
+		# redirect_to 
+	end
+end
+
