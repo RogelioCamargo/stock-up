c2608d598ee54ff2765718ac25ec2c99b595d35b
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-06-25 12:58:19 -0700

    Create routes for user and session

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+	resources :users, only: %i(new create show)
+	resource :session, only: %i(new create destroy)
 end
