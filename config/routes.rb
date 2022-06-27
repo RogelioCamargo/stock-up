05f8ff4974316776e7c24dd8a157655399057a4c
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-06-27 08:28:47 -0700

    Ensure bootstrap javascripts works with rails

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "products#index"
 	resources :users, only: %i(new create show)
 	resource :session, only: %i(new create destroy)
 	resources :categories, except: %i(new)
