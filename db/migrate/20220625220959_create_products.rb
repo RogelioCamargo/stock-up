73ef166cd53f5cabe2be6fb95438e3e87d7ee960
RogelioCamargo <rogelio.camargo2016@gmail.com>
2022-06-30 09:19:38 -0700

    Display header buttons on the right side

--- db/migrate/20220625220959_create_products.rb
+++ db/migrate/20220625220959_create_products.rb
@@ -4,9 +4,12 @@ class CreateProducts < ActiveRecord::Migration[7.0]
 			t.string :name, null: false
 			t.integer :quantity
 			t.integer :status, null: false, default: 0
-			t.string :image_url
 			t.string :shop_url
 			t.integer :category_id 
+			t.integer :user_id
+			t.integer :trigger_alert_amount 
+			t.integer :trigger_warning_amout
+			t.integer :reorder_amount
 
       t.timestamps
     end
